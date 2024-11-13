4252054d57f5df428ae0bd42a00b7fed3657034e
Adrien THEOPHILE <161586262+ByAdri@users.noreply.github.com>
2024-11-13 16:59:02 +0100

    Ajout du code pyhton de création de fichier

--- Iot/main.py
+++ Iot/main.py
@@ -1,7 +1,11 @@
+import ast
+
+import paho.mqtt.client as mqtt
 import json
 import logging
 import configparser
-import paho.mqtt.client as mqtt
+import os
+
 print("Lancement ")
 
 # Charger la configuration depuis le fichier config.ini
@@ -19,51 +23,93 @@ topic_panneaux = config['topics']['sub_panneaux']
 
 logging.basicConfig(level=logging.INFO)
 
-# Structure pour stocker les données récupérées
-data = {
-    'panneaux': [],
-    'salles': []
-}
+donnees = ast.literal_eval(config['data']['donnees'])  # Convertir la chaîne en une liste
+
+seuil_max = ast.literal_eval(config['seuil']['seuil_max'])
+seuil_min = ast.literal_eval(config['seuil']['seuil_min'])
+
 
 # Callback de réception des messages
 def get_data(mqttc, obj, msg):
     try:
+        # print(msg.payload)
+
         # Désérialiser le message reçu
         jsonMsg = json.loads(msg.payload)
 
         if msg.topic == topic_panneaux:
+
+            room = 'solar'
             energy = jsonMsg['lifeTimeData']['energy']
             power = jsonMsg['currentPower']['power']
-            data['panneaux'].append([energy, power])
-            print("Dernières infos des panneaux solaires :")
+            print("\nDernières infos des panneaux solaires :")
             print(f"Energie : {energy}")
-            print(f"Power : {power}\n")
+            print(f"Power : {power}")
+            save_data_to_file_solar(room, energy, power)
+
+
 
         else:
-            temperature = jsonMsg[0]['temperature']
             room = jsonMsg[1]['room']
-            data['salles'].append([room, temperature])
-            print(f"Room: {room}")
-            print(f"Temperature: {temperature}\n")
+            print(f"\nRoom: {room}")
+            data = {}
 
-        # Écriture des données dans un fichier texte après mise à jour
-        save_data_to_file()
+            for i in range(len(donnees)):
+                donnee = jsonMsg[0][donnees[i]]
+                data[donnees[i]] = donnee
+
+            print(data)
+            save_data_to_file( room, data)
+
+
+
 
     except (json.JSONDecodeError, KeyError) as e:
         logging.error("Erreur dans les données reçues : %s", e)
 
-# Fonction pour sauvegarder les données formatées dans un fichier texte
-def save_data_to_file():
-    output = ""
-    for key, values in data.items():
-        formatted_key = 'B202' if key == 'panneaux' else 'B201'
-        formatted_values = ''.join([f"[{','.join(map(str, item))}]" for item in values])
-        output += f"{formatted_key}{formatted_values}"
-    
-    with open("output.txt", "w") as file:
-        file.write(output)
 
-    print("Données enregistrées dans le fichier output.txt")
+def save_data_to_file(room, donnees: dict):
+
+    file_name = "salles.json"
+
+    if os.path.exists(file_name):
+        with open(file_name, "r") as file:
+            data = json.load(file)
+    else :
+        data = {}
+    
+    
+    if room not in data:
+        data[room] = {}
+
+    suivant = str(len(data[room]))
+    data[room][suivant] = donnees
+
+    
+    with open(file_name, "w") as file:
+        json.dump(data, file, indent=1)
+
+def save_data_to_file_solar(room, energy, power):
+    file_name = f"{room}.json"
+
+    if os.path.exists(file_name):
+        with open(file_name, "r") as file:
+            data = json.load(file)       
+    else:
+        data = []
+
+    # Ajouter les nouvelles données sous forme d'un dictionnaire
+    new_data = {}
+    new_data["energy"] = energy
+    new_data["power"] = power
+    data.append(new_data)
+
+    # Sauvegarder les données dans le fichier
+    with open(file_name, "w") as file:
+        json.dump(data, file, indent=2)
+
+    print(f"Données de la salle {room} enregistrées dans le fichier {file_name}")
+
 
 # Connexion et souscription
 mqttc = mqtt.Client()
