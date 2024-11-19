e857883c64c4ca4c07ee1674ef45c230d744fe0b
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-11-19 15:23:18 +0100

    rectif

--- Application/app/src/main/java/sae/Main.java
+++
@@ -1,8 +0,0 @@
-package sae;
-
-public class Main {
-    public static void main(String[] args) {
-        // Appelle launch() pour démarrer l'application JavaFX
-        App.launch(args);
-    }
-}
