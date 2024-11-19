a9cbdfd8a1eda6a25c6a9e1421e427f130784e0d
Adrian G <adrianguerrero0903@gmail.com>
2024-11-19 10:58:35 +0100

    Test fonctionnel d'un stage

--- Application/app/src/main/java/sae/App.java
+++ Application/app/src/main/java/sae/App.java
@@ -1,19 +1,25 @@
 package sae;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class App extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Stage stage = new Stage();
+        Scene scene = new Scene(new BorderPane(), 400, 400);
+        stage.setScene(scene);
+        
         System.out.println("La version de Java utilisée est :");
         System.out.println( System.getProperty("java.version") );
         System.out.println("Fin du programme");
+        stage.show();
     }
-    public static void main(String[] args) {
-       
-        
+    public static void main2(String[] args) {
+       Application.launch(args);   
     }
 
     
