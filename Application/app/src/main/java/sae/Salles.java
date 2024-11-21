e9e10138e1850c601320c30397b6bf93e649b62f
kwadjanib <bilon.kwadjani@etu.univ-tlse2.fr>
2024-11-21 10:49:15 +0100

    Class salle.jav

---
+++ Application/app/src/main/java/sae/Salles.java
@@ -0,0 +1,16 @@
+package sae;
+public class Salles {
+    private String nom;
+    private double temperature;
+    private double humidite;
+    private int co2;
+
+    public Salles(String nom, double temperature, double humidite, int co2) {
+        this.nom = nom;
+        this.temperature = temperature;
+        this.humidite = humidite;
+        this.co2 = co2;
+    }
+    
+    
+}
