16751720012f0955875023899e45b0eb9a8c8f34
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-11-21 11:48:59 +0100

    Ajout des interactions avec les choix

--- Application/app/src/main/java/sae/view/ParametrageChoixSalles.java
+++ Application/app/src/main/java/sae/view/ParametrageChoixSalles.java
@@ -1,11 +1,17 @@
 package sae.view;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
 import sae.App;
 import sae.appli.TypeDonnee;
@@ -18,11 +24,15 @@ public class ParametrageChoixSalles {
     private Button boutton ;
     @FXML
     private Button butRetour ;
+    @FXML
+    private Button butValider ;
 
     @FXML
     private MenuButton choixTypeDonnees;
 
     private App application;
+
+    ArrayList<String> choices = new ArrayList<>();
 
 
     public void setDatas(Stage fenetre,  App app) {
@@ -46,6 +56,19 @@ public class ParametrageChoixSalles {
     private void actionRetour() {
 		  application.loadMenu();
 	  }
+
+    @FXML
+    private void actionValider() {
+		  actionChoix();
+	  }
+
+    public void actionChoix () {
+      ObservableList<MenuItem> obList=choixTypeDonnees.getItems();
+      for(MenuItem n : obList){
+          if(((CheckMenuItem)n).isSelected())
+            choices.add(n.getText());
+      }
+    }
 
 
 }
