1b09d86fbd06aeea7a37fcda179eda264a65986a
kwadjanib <bilon.kwadjani@etu.univ-tlse2.fr>
2024-11-26 11:53:52 +0100

    modif

--- Application/app/src/main/java/sae/appli/Salle.java
+++ Application/app/src/main/java/sae/appli/Salle.java
@@ -5,23 +5,9 @@ import java.util.ArrayList;
 
 public class Salle {
     private String nom;
-    private ArrayList<TypeDonnee> donnees;
 
     public Salle(String nom){
         this.nom = nom;
-        this.donnees = new ArrayList<TypeDonnee>();
-    }
-    public String getNom(){
-        return this.nom;
-    }
-    public void ajouterDonnee(TypeDonnee donnee){
-        this.donnees.add(donnee);
-    }
-    public void retirerDonnee(TypeDonnee donnee){
-        this.donnees.remove(donnee);
-    }
-    public ArrayList<TypeDonnee> getDonnees(){
-        return this.donnees;
     }
     public String toString() {
         return this.nom;
