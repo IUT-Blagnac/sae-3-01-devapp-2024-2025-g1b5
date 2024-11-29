1b95996a83b308fcecd1ce3505f72a8cc3b8f3f9
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-11-29 13:59:12 +0100

    temp

--- Application/app/src/main/java/sae/App.java
+++ Application/app/src/main/java/sae/App.java
@@ -22,22 +22,26 @@ import sae.view.ParametrageChoixSalles;
 import sae.view.ParametrageSolar;
 import sae.view.SallesConfigController;
 import sae.view.SolarConfigController;
+import sae.view.AlarmesController;
 
-public class App extends Application{
+import sae.appli.MqttAlarmListener;
+import sae.view.AlarmPopUpController;
+
+
+public class App extends Application {
 
     private BorderPane rootPane;
     private Stage stage;
     private Process pythonProcess;
+    private boolean isAlarmListenerActive = true;  // Variable de contrôle pour le thread des alarmes
 
 
-
-    //partager des données entre controllers
+    // Partager des données entre controllers
     private String numSalle;
-    ArrayList<String> donneesChoisies = new ArrayList<>();
-
+    private ArrayList<String> donneesChoisies = new ArrayList<>();
 
     @Override
-    public void start(Stage primaryStage)  {
+    public void start(Stage primaryStage) {
         this.stage = primaryStage;
         this.rootPane = new BorderPane();
         Scene scene = new Scene(rootPane);
@@ -46,9 +50,20 @@ public class App extends Application{
         loadMenu();
         primaryStage.setTitle("Menu");
         primaryStage.show();
+
+        // Lancer l'écoute des alarmes MQTT
+        startMqttListener();
         startPythonScript();
 
+        primaryStage.setOnCloseRequest(event -> {
+            // Arrêter les alarmes et fermer proprement l'application
+            stopAlarmListener();
+            stopPythonProcess();  // Arrêter le processus Python
+            System.exit(0);  // Fermer l'application
+        });
     }
+
+    
 
     private void startPythonScript() {
         Thread pythonThread = new Thread(() -> {
@@ -70,26 +85,50 @@ public class App extends Application{
         pythonThread.start();
     }
 
+    // Méthode pour démarrer l'écouteur MQTT
+    private void startMqttListener() {
+        MqttAlarmListener mqttListener = new MqttAlarmListener();
+        mqttListener.start();
+    
+        // Vérifier périodiquement si le fichier trigger.flag a été mis à jour
+        new Thread(() -> {
+            while (isAlarmListenerActive) { // Le thread continue tant que cette variable est true
+                checkForAlarms();
+                try {
+                    Thread.sleep(5000); // Vérifier toutes les 5 secondes
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+    
+    public void stopAlarmListener() {
+        isAlarmListenerActive = false;  // Cette ligne arrête le thread
+    }
+    
+
+    // Méthode pour vérifier si une alarme a été déclenchée
+    private void checkForAlarms() {
+        AlarmPopUpController alarmPopUpController = new AlarmPopUpController();
+        alarmPopUpController.showAlarmPopUp();
+    }
+
     public void loadMenu() {
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(App.class.getResource("view/menu.fxml"));
 
             BorderPane vueListe = loader.load();
-
             MenuController menu = loader.getController();
             menu.setDatas(stage, this);
-            
-            this.rootPane.setCenter(vueListe);
-            
 
+            this.rootPane.setCenter(vueListe);
         } catch (IOException e) {
             System.out.println("Ressource FXML non disponible : menu.fxml");
             System.exit(1);
         }
     }
-
-    
 
     public void loadParametrageSalles() {
         try {
@@ -98,7 +137,6 @@ public class App extends Application{
             BorderPane vueListe = loader.load();
 
             TypeDonnee[] donnees = TypeDonnee.values();
-            // Convertir en listes
             List<TypeDonnee> listTypeDonnee = Arrays.asList(donnees);
 
             ParametrageChoixSalles choixSalles = loader.getController();
@@ -106,11 +144,10 @@ public class App extends Application{
 
             this.numSalle = choixSalles.getSalle();
             this.donneesChoisies = choixSalles.getTabDonnee();
-            
-            choixSalles.loadMenuDeroulantDonnees(listTypeDonnee);
-            
-            this.rootPane.setCenter(vueListe);
 
+            choixSalles.loadMenuDeroulantDonnees(listTypeDonnee);
+
+            this.rootPane.setCenter(vueListe);
         } catch (IOException e) {
             System.out.println("Ressource FXML non disponible : salles.fxml");
             System.exit(1);
@@ -125,9 +162,8 @@ public class App extends Application{
 
             ParametrageSolar choixSolar = loader.getController();
             choixSolar.setDatas(stage, this);
-            
-            this.rootPane.setCenter(vueListe);
 
+            this.rootPane.setCenter(vueListe);
         } catch (IOException e) {
             System.out.println("Ressource FXML non disponible : solar.fxml");
             System.exit(1);
@@ -158,15 +194,13 @@ public class App extends Application{
 
             SolarConfigController configSolar = loader.getController();
             configSolar.setDatas(stage, this);
-            
-            this.rootPane.setCenter(vueListe);
 
+            this.rootPane.setCenter(vueListe);
         } catch (IOException e) {
             System.out.println("Ressource FXML non disponible : solarConfig.fxml");
             System.exit(1);
         }
     }
-
 
     public void loadMenuConfig() {
         try {
@@ -176,32 +210,30 @@ public class App extends Application{
 
             ConfigController config = loader.getController();
             config.setDatas(stage, this);
-            
-            this.rootPane.setCenter(vueListe);
 
+            this.rootPane.setCenter(vueListe);
         } catch (IOException e) {
             System.out.println("Ressource FXML non disponible : menuConfig.fxml");
             System.exit(1);
         }
     }
 
-    public void loadDonnees(){
-      try {
+    public void loadDonnees() {
+        try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(App.class.getResource("view/donnee.fxml"));
 
             BorderPane vueListe = loader.load();
 
             AfficherDonneesController affichage = loader.getController();
-            
+
             affichage.setDatas(stage, this);
             affichage.setSalle(this.numSalle);
             affichage.setTab(donneesChoisies);
-            
-            affichage.afficherDonnees();
-            
-            this.rootPane.setCenter(vueListe);
 
+            affichage.afficherDonnees();
+
+            this.rootPane.setCenter(vueListe);
         } catch (IOException e) {
             System.out.println("Ressource FXML non disponible : donnee.fxml");
             System.exit(1);
@@ -273,8 +305,34 @@ public class App extends Application{
     }
 
 
-    public static void main2(String[] args) {
-        Application.launch(args);   
-    }
 
+    public void loadAlarmes() {
+    try {
+        // Création d'un FXMLLoader pour charger le fichier FXML
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(App.class.getResource("view/alarmes.fxml"));
+        
+        // Chargement de la vue définie dans alarmes.fxml
+        BorderPane vueListe = loader.load();
+        
+        // Obtention du contrôleur associé à la vue
+        AlarmesController alarmesController = loader.getController();
+        
+        // Transmission des données nécessaires au contrôleur
+        alarmesController.setDatas(stage, this);
+        
+        // Remplacement du contenu central de rootPane par la vue chargée
+        this.rootPane.setCenter(vueListe);
+    } catch (IOException e) {
+        // Gestion des erreurs si le fichier FXML est introuvable ou mal configuré
+        System.out.println("Ressource FXML non disponible : alarmes.fxml");
+        e.printStackTrace();
+        System.exit(1);
+    }
+}
+
+
+    public static void main2(String[] args) {
+        Application.launch(args);
+    }
 }
