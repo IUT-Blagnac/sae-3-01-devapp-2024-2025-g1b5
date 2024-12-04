f4e553c6abf0936fd296710163ad95d2be7c0732
kwadjanib <bilon.kwadjani@etu.univ-tlse2.fr>
2024-12-04 15:25:08 +0100

    test faker

--- SiteSAE/ListeProduit.php
+++ SiteSAE/ListeProduit.php
@@ -1,4 +1,28 @@
 <?php
 include 'header.php';
-include 'TableauxProduit.php';
+include 'tableauxProduit.php';
+
+if($_GET['idCategorie']){
+    $idCategorie = $_GET['idCategorie'];
+    $produit = getProduitByCategorie($idCategorie);
+}
+if($_GET['rechercheProduit']){
+    $rechercheProduit = $_GET['rechercheProduit'];
+    $produit = getProduit($rechercheProduit);
+}
+if($_GET['age']){
+    $age = $_GET['age'];
+    $produit = getProduitByAge($age);
+}
+
+if($_GET['promo']){
+    $promo = $_GET['promo'];
+    $produit = getProduitByPromo($promo);
+}
+
+
 ?>
+
+
+    
+
--- SiteSAE/tableauxProduit.php
+++ SiteSAE/tableauxProduit.php
@@ -1,45 +1,15 @@
 <?php
+require_once 'vendor/autoload.php';
 include 'Connect.inc.php';
-$produit=[];
-$prodRecherche = [];
-$prodAge = [];
-$prodPromo = [];
-$prodType = [];
-$prodBestSeller = [];
 
-$donneeProd=$pdo->prepare("SELECT * FROM Produits");
-if($donneeProd->execute()){
-    foreach($donneeProd as $prod){
-        $produit[]=[
-            'idProduit'=>$prod['idProduit'],
-            'idCategorie'=>$prod['idCategorie'],
-            'nomProduit'=>$prod['nomProduit'],
-            'prixProduit'=>$prod['prixProduit']
-        ];
-    }
-}
-
-function getProduit($rechercheProduit){
-    global $produit;
-    foreach($produit as $prod){
-        if($rechercheProduit.contains($prod['nomProduit']) || $rechercheProduit.contains($prod['idProduit'])){ 
-            $prodRecherche[] = $prod;
-        }
-    }
-    return null;
-}
-function getProduitByCategorie($idCategorie){
-    global $produit;
-    foreach($produit as $prod){
-        if($prod['idCategorie']==$idCategorie){
-            $prodType[] = $prod;
-        }
-    }
-    return $tab;
-}
+$faker = Faker\Factory::create();
+$tabProduit=[];
+$tabCategorie=[];
 
 
+ 
 
+$client = []
 
 
 
