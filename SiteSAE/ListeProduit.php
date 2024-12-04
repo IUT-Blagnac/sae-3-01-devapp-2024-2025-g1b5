cf4b18cb5c22ec7ab6903680e248f670c11099d3
kwadjanib <bilon.kwadjani@etu.univ-tlse2.fr>
2024-12-04 15:43:49 +0100

    client

--- SiteSAE/ListeProduit.php
+++ SiteSAE/ListeProduit.php
@@ -2,23 +2,8 @@
 include 'header.php';
 include 'tableauxProduit.php';
 
-if($_GET['idCategorie']){
-    $idCategorie = $_GET['idCategorie'];
-    $produit = getProduitByCategorie($idCategorie);
-}
-if($_GET['rechercheProduit']){
-    $rechercheProduit = $_GET['rechercheProduit'];
-    $produit = getProduit($rechercheProduit);
-}
-if($_GET['age']){
-    $age = $_GET['age'];
-    $produit = getProduitByAge($age);
-}
-
-if($_GET['promo']){
-    $promo = $_GET['promo'];
-    $produit = getProduitByPromo($promo);
-}
+var_dump($tabProduit);
+var_dump($tabCategorie);
 
 
 ?>
--- SiteSAE/tableauxProduit.php
+++ SiteSAE/tableauxProduit.php
@@ -2,14 +2,31 @@
 require_once 'vendor/autoload.php';
 include 'Connect.inc.php';
 
-$faker = Faker\Factory::create();
+$faker = Faker\Factory::create('fr_FR');
 $tabProduit=[];
 $tabCategorie=[];
+$tabClient=[];
+
+for($i=0;$i<10;$i++){
+    $tabClient = [
+        'idClient' => $faker->randomDigitNotNull,
+        'mailClient' => $faker->email,
+        'nomClient' => $faker->name,
+        'prenomClient' => $faker->name,
+        'telClient' => $faker->phoneNumber,
+        'mdpClient' => $faker->password,
+        'genre' => $faker->randomElement($array = array ('H','F')),
+        'dateNaissance' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'role'=> 'client',
+        'idAdresse' => $faker->randomDigitNotNull
+    ];
+}
+var_dump($tabClient);
 
 
  
 
-$client = []
+$client = [];
 
 
 
