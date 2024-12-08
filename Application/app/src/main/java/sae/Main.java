5e213d4f3e53931abc7bd4e45ca21cdc5b80ab2b
RABAN Théo <148774817+Theor31@users.noreply.github.com>
2024-12-08 19:16:20 +0100

    supress

--- Application/app/src/main/java/sae/Main.java
+++
@@ -1,9 +0,0 @@
-package sae;
-
-public class Main {
-    public static void main(String[] args) {
-        String userDirectoryPath = System.getProperty("user.dir");
-		System.out.println("Current Directory = \"" + userDirectoryPath + "\"" );
-        App.main2(args);
-    }
-}
